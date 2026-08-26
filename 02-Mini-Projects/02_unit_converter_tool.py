9d15af1b8e1d59b386e098e525cdd92f0e3979eb
maielafandy <maialafandy@gmail.com>
2026-08-26 13:13:46 +0300

    Refactored by functions
    Refactored the project by introducing functions
    to improve code organization, readability, and reusability.

--- 02-Mini-Projects/02_unit_converter_tool.py
+++ 02-Mini-Projects/02_unit_converter_tool.py
@@ -1,75 +1,99 @@
 # ---------------------------------
 # This file is for learning
 # creator: Mai Mohamed
-# creation date : 5/28/2026
+# creation date: 5/28/2026
+# modification date: 6/26/2026
 # ---------------------------------
 
-while True:
+
+def Celisuis_to_fehinrhiet(value):
+    return (value*9/5)+32
+
+
+def fehinrhiet_to_Celisuis(value):
+    return (value-32)*5/9
+
+
+def meter_to_centimeter(value):
+    return value*100
+
+
+def centimeter_to_meter(value):
+    return value/100
+
+
+def take_type_of_units():
+
     try:
-        type_of_unit = int(
-            input("choose the type of unit\n1-length\n2-tempreture\n3-exit\n"))
-        if type_of_unit == 3:
-            break
-        elif type_of_unit == 1:
-            try:
-                Conversion = int(
-                    input("choose the type of unit\n1- m-->cm\n2- cm-->m\n3-km-->m\n"))
-                if Conversion == 1:
-                    try:
-                        Num = float(input("enter a number:\n"))
-                        Result = Num*100
-                    except:
-                        print("the input not valid ")
-                        break
+        type_of_units = int(
+            input("choose the type of units \n1-tempreture \n2-length\n3-exit"))
 
-                elif Conversion == 2:
+    except ValueError:
+        print("invalid input\n")
+    return type_of_units
 
-                    try:
-                        Num = float(input("enter a number:\n"))
-                        Result = Num/100
-                    except:
-                        print("the input not valid ")
-                        break
 
-                elif Conversion == 3:
-                    try:
-                        Num = float(input("enter a number:\n"))
-                        Result = Num*1000
-                    except:
-                        print("the input not valid ")
-                        break
+def take_the_conversion():
 
-            except:
-                print("your choice is not valid ")
-                break
-        elif type_of_unit == 2:
-            try:
-                Conversion = int(input(
-                    "choose the type of unit\n1- Celisuis --> fehinrhiet \n2-fehinrhiet --> Celisuis\n "))
-                if Conversion == 1:
-                    try:
-                        Num = float(input("enter a number:\n"))
-                        Result = (Num*9/5)+32
-                    except:
-                        print("the input not valid ")
-                        break
+    if type_of_units == 1:
 
-                elif Conversion == 2:
+        try:
+            type_of_coversion = int(input(
+                "choose the type of unit\n1- Celisuis --> fehinrhiet \n2-fehinrhiet --> Celisuis\n "))
+        except ValueError:
+            print("invalid input\n")
+        return type_of_coversion
 
-                    try:
-                        Num = float(input(("enter a number:\n")))
-                        Result = (Num-32)*5/9
-                    except:
-                        print("the input not valid ")
-                        break
-            except:
-                print("your choice is not valid ")
-                break
+    elif type_of_units == 2:
+
+        try:
+            type_of_coversion = (int(
+                input("choose the type of unit\n1- m-->cm\n2- cm-->m\n")))
+
+        except ValueError:
+            print("invalid input\n")
+        return type_of_coversion
+    else:
+        print("invalid choice")
+        return None
+
+
+def take_the_value():
+
+    try:
+        value = float(input("enter a number:\n"))
+    except ValueError:
+        print("invalid input\n")
+    return value
+
+
+def choose_the_operation(type_of_units, type_of_coversion, value):
+
+    if (type_of_units == 1):
+        if (type_of_coversion == 1):
+            return Celisuis_to_fehinrhiet(value)
+        elif (type_of_coversion == 2):
+            return fehinrhiet_to_Celisuis(value)
         else:
-            print("your choice is not valid ")
-            break
-        print(Result)
-        print("\n")
-    except:
-        print("your choice is not valid ")
+            print("invalid choice")
+            return None
+    elif (type_of_units == 2):
+        if (type_of_coversion == 1):
+            return meter_to_centimeter(value)
+        elif (type_of_coversion == 2):
+            return centimeter_to_meter(value)
+        else:
+            print("invalid choice")
+            return None
+
+
+while True:
+    type_of_units = take_type_of_units()
+    if type_of_units == 3:
         break
+    type_of_coversion = take_the_conversion()
+    if type_of_coversion == 3:
+        break
+    value = take_the_value()
+    result = choose_the_operation(type_of_units, type_of_coversion, value)
+    print(f'the result is {result}')
